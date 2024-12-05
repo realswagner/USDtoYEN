3fead6b8f5f387f0d1c1db21d7c343f3e23ab2c2
realswagner <24cm0120@jec.ac.jp>
2024-12-05 17:42:45 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter date in YY/DD/MM format");
+        String dateInput = scan.nextLine();
+
+        if
+    }
+}
