5f38a1e6da0ebfe0d82afd8713b144923144021d
realswagner <24cm0120@jec.ac.jp>
2024-12-05 19:36:23 +0900

    Updated logic for validating date

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter date in YY/DD/MM format");
-        String dateInput = scan.nextLine();
+        boolean dateValid = false;
+        System.out.println("Please enter date in dd/mm/yyyy format");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        while(!dateValid) {
+            Scanner scan = new Scanner(System.in);
+            String dateInput = scan.nextLine();
+            try {
+                LocalDate parsedDate = LocalDate.parse(dateInput, formatter);
+                if (parsedDate.isAfter(LocalDate.now()) ||
+                        parsedDate.isBefore(LocalDate.of(1960, 1, 1))) {
+                    System.out.println("Please enter valid date between 01/01/1960 and now");
+                } else {
+                    System.out.println("Date entered is valid " + parsedDate);
+                    dateValid = true;
+                }
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid date format try again.");
+            }
+        }
+        }
 
-        if
     }
-}
